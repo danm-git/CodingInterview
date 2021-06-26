892e8452a9094b8923821a427ffc8a8a527d3c7a
Dan <daniel.e.munro@gmail.com>
2021-06-25 22:22:37 -0600

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+    /*let outputString = '';
+
+    //console.log('steps(' + n + ')');
+    for(let i = 0; i < n; i++) {
+        for(let x = 0; x < n; x++) {
+            if(x <= i) {
+                outputString += '#';
+            } else {
+                outputString += ' ';
+            }
+        }
+
+        //console.log('"' + outputString + '"');
+        console.log(outputString);
+        outputString = '';
+    }*/
+
+    if (n === row) {
+        return;
+    }
+
+    if (n == stair.length) {
+        console.log(stair);
+        return steps(n, row +1);
+    }
+
+    if(stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+}
 
 module.exports = steps;
