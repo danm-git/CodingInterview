54f563d5c949108bef8ca07ba3e5314998c4b2e8
Dan <daniel.e.munro@gmail.com>
2021-06-25 19:58:54 -0600

    anagram2

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -63,5 +63,12 @@ function buildCharMap(str) {
     return charMap;
 }
 
+function anagrams2(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(stringA, stringB) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
